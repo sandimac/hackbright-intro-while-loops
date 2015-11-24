c2d2a7b94fcb34b5dedeb952fc3a924dd7c57e7d
Whitney&Gulmira <W&G@msn.com>
2015-11-23 21:08:07 -0800

    adding while loops file

---
+++ while_fun_loops.py
@@ -0,0 +1,64 @@
+# num = 21
+
+
+# index = 0
+# while (index<num):
+# 	if index == 13:
+# 		print "hello"
+# 	else:
+# 		print index
+# 	index +=1
+	
+
+# num = 101
+# index = 0 
+# while (index<num):
+# 	print index
+# 	index +=10
+
+# index = index + 10
+
+# num = 11 
+# index = 0 
+# while (index<num):
+# 	if (index%2==0):
+# 		index +=1
+# 	else:
+# 		print index
+# 		index+=1
+
+
+# num = 11
+# index = 10
+# while (index<num):
+# 	if (index == 0):
+# 		print "BLASTOFF"
+# 		break
+# 	else:
+# 		print index
+# 		index-=1
+
+
+# fruits = ['apples', 'oranges', 'bananas']
+# index = 0
+# while (index<len(fruits)):
+# 	print fruits[index]
+# 	index +=1
+
+
+
+num = 3
+
+
+def sum_nums(num):
+	index = 0
+	sum = 0
+	while (index<= num):
+		sum = sum + index
+		index += 1
+	print sum
+		
+sum_nums(num)
+
+
+
